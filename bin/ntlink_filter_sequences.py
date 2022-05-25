0f9701cd43bbd3150a9ef311c56afbe588560a20
lcoombe <lauren.e.coombe@gmail.com>
2022-05-25 11:12:12 -0700

    Revert initial internal masking logic

--- bin/ntlink_filter_sequences.py
+++ bin/ntlink_filter_sequences.py
@@ -11,22 +11,11 @@ from read_fasta import read_fasta
 import ntlink_utils
 import ntlink_overlap_sequences
 
-def filter_sequences(fa_in, valid_mx_regions, mask_internal=False):
+def filter_sequences(fa_in, valid_mx_regions):
     "Filter the input fasta file to only output those in valid_mx_regions"
     with open(fa_in, 'r') as fin:
         for name, seq, _, _ in read_fasta(fin):
             if name in valid_mx_regions:
-                if mask_internal:
-                    valid_regions = sorted(valid_mx_regions[name])
-                    five_prime_coord, three_prime_coord = 0, len(seq)
-                    for region in valid_regions:
-                        if region[0] == 0: # 5' end
-                            five_prime_coord = region[1]
-                        if region[1] == len(seq): # 3' end
-                            three_prime_coord = region[0]
-                    assert len(valid_regions) > 0 and len(valid_regions) < 3
-                    new_seq = seq[:five_prime_coord] + 'N'*(three_prime_coord - five_prime_coord) + seq[three_prime_coord:]
-                    assert len(new_seq) == len(seq)
                 print(">{}\n{}".format(name, seq))
 
 def main():
@@ -38,7 +27,6 @@ def main():
     parser.add_argument("-k", help="K-mer size (bp)", required=True, type=int)
     parser.add_argument("-f", help="Fudge factor for estimated overlap [0.5]", type=float, default=0.5)
     parser.add_argument("-g", help="Minimum gap size (bp) [20]", required=False, type=int, default=20)
-    parser.add_argument("--mask", help="Mask internal region of sequences", required=False, action="store_true")
     parser.add_argument("-v", "--version", action='version', version='ntLink v1.2.1')
 
     args = parser.parse_args()
@@ -50,7 +38,7 @@ def main():
         scaffolds = ntlink_overlap_sequences.read_fasta_file_trim_prep(args.fasta)
         valid_mx_regions = ntlink_utils.find_valid_mx_regions(args, gap_re, graph, scaffolds)
 
-    filter_sequences(args.fasta, valid_mx_regions, mask_internal=args.mask)
+    filter_sequences(args.fasta, valid_mx_regions)
 
 
 if __name__ == "__main__":
