6e9105ca31bbeb08e5dd30c797011392e635820c
lcoombe <lauren.e.coombe@gmail.com>
2022-12-16 14:36:22 -0800

    Prepare for release v1.3.7

--- bin/ntlink_filter_sequences.py
+++ bin/ntlink_filter_sequences.py
@@ -51,7 +51,7 @@ def main():
     parser.add_argument("-f", help="Fudge factor for estimated overlap [0.5]", type=float, default=0.5)
     parser.add_argument("-g", help="Minimum gap size (bp) [20]", required=False, type=int, default=20)
     parser.add_argument("-t", help="Number of threads", default=8, type=int)
-    parser.add_argument("-v", "--version", action='version', version='ntLink v1.3.6')
+    parser.add_argument("-v", "--version", action='version', version='ntLink v1.3.7')
 
     args = parser.parse_args()
 
